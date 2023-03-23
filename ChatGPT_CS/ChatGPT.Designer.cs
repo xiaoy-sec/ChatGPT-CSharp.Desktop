6053dcc377744161af054cc27da15099f6ddb363
xiaoy-sec <782790134@qq.com>
2023-03-23 23:36:06 +0800

    提交

---
+++ ChatGPT_CS/ChatGPT.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ChatGPTCS
+{
+    partial class ChatGPT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.cbModel = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSend.Location = new System.Drawing.Point(387, 227);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(109, 41);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Label4
+            // 
+            this.Label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(35, 240);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(92, 15);
+            this.Label4.TabIndex = 19;
+            this.Label4.Text = "选择Model：";
+            // 
+            // cbModel
+            // 
+            this.cbModel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbModel.FormattingEnabled = true;
+            this.cbModel.Items.AddRange(new object[] {
+            "text-davinci-003",
+            "gpt-3.5-turbo"});
+            this.cbModel.Location = new System.Drawing.Point(133, 237);
+            this.cbModel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbModel.Name = "cbModel";
+            this.cbModel.Size = new System.Drawing.Size(239, 23);
+            this.cbModel.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 25);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.Text = "sk-";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 25);
+            this.textBox2.TabIndex = 26;
+            this.textBox2.Text = "127.0.0.1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(447, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(95, 25);
+            this.textBox3.TabIndex = 27;
+            this.textBox3.Text = "10809";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(548, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 41);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "getModels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "API：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "代理IP：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "代理端口：";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(542, 227);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 41);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "结束当前会话";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 659);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "状态：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 659);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "等待中";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(423, 659);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(234, 15);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "如果等很久没回显，再次Send一下";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 90);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(632, 132);
+            this.richTextBox1.TabIndex = 37;
+            this.richTextBox1.Text = "say hello";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(21, 277);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(632, 367);
+            this.richTextBox2.TabIndex = 38;
+            this.richTextBox2.Text = "";
+            // 
+            // ChatGPT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(675, 687);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.cbModel);
+            this.Controls.Add(this.btnSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ChatGPT";
+            this.Text = "Chat GPT";
+            this.Load += new System.EventHandler(this.ChatGPT_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Button btnSend;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.ComboBox cbModel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        internal System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ ChatGPT_CS/ChatGPT.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Drawing;
+
+namespace ChatGPTCS
+{
+    public partial class ChatGPT : Form
+    {
+
+        public ChatGPT()
+        {
+            InitializeComponent();
+        }
+        private List<string> messageHistory = new List<string>();
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            label5.Text = "发送请求中...";
+            string sayWord = richTextBox1.Text;
+            if (string.IsNullOrEmpty(sayWord))
+            {
+                MessageBox.Show("说点什么");
+                richTextBox1.Focus();
+            }
+            if (richTextBox2.Text != "")
+            {
+                richTextBox2.AppendText("\r\n");
+            }
+            richTextBox2.AppendText("Me: " + sayWord + "\r\n");
+            try
+            {
+                Task<string> sAnswer = SendMsg(textBox1.Text, sayWord, textBox2.Text, textBox3.Text,cbModel.Text);
+                sAnswer.ContinueWith(t => {
+                    string result = t.Result;
+                    this.Invoke((MethodInvoker)delegate {
+                        richTextBox2.SelectionColor = Color.Red;
+                        richTextBox2.AppendText("Chat GPT: " + result);
+                        label5.Text = "请求完成，等待下次请求。";
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("出错了: " + ex.Message);
+            }
+            messageHistory.Add(sayWord);
+
+        }
+
+        
+        public async Task<string> SendMsg(string API,string askQuestion,string proxyIP,string proxyPort,string model)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = 
+                System.Net.SecurityProtocolType.Ssl3 | 
+                System.Net.SecurityProtocolType.Tls12 | 
+                System.Net.SecurityProtocolType.Tls11 | 
+                System.Net.SecurityProtocolType.Tls;
+            var proxy = new WebProxy($"http://{proxyIP}:{proxyPort}");
+
+            var clientHandler = new HttpClientHandler()
+            {
+                Proxy = proxy,
+                UseProxy = true
+            };
+            var client = new HttpClient(clientHandler);
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + API);
+            if (model ==null)
+            {
+                MessageBox.Show("先获取model");
+            }
+            else if (model.StartsWith("gpt"))
+            {
+                var data = new
+                {
+                    model = model,
+                    messages = messageHistory.Select(message => new { role = "system", content = message }).Concat(new[] { new { role = "user", content = askQuestion } }),
+                    temperature = 0.7
+                };
+
+
+                var content = new StringContent(JsonConvert.SerializeObject(data));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
+                using (Stream responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    using (StreamReader streamReader = new StreamReader(responseStream))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            string message = await streamReader.ReadLineAsync();
+                            JObject jsonObject = JObject.Parse(message);
+                            string contentValue = jsonObject["choices"][0]["message"]["content"].ToString();
+                            contentValue = string.Join(Environment.NewLine, contentValue.Split('\n'));
+                            messageHistory.Add(contentValue);
+                            return contentValue;
+                        }
+                    }
+                }
+            }
+            else if (model == "text-davinci-003" || model == "text-davinci-002" || model == "text-curie-001" || model == "text-babbage-001" || model == "text-ada-001")
+            {
+                var data = new
+                {
+                    model = model,
+                    prompt = askQuestion,
+                    temperature = 0.7
+                };
+                var content = new StringContent(JsonConvert.SerializeObject(data));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+                using (Stream responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    using (StreamReader streamReader = new StreamReader(responseStream))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            string message = await streamReader.ReadLineAsync();
+                            JObject jsonObject = JObject.Parse(message);
+                            string contentValue = jsonObject["choices"][0]["text"].ToString();
+                            contentValue = string.Join(Environment.NewLine, contentValue.Split('\n'));
+                            messageHistory.Add(contentValue);
+                            return contentValue;
+                        }
+                    }
+                }
+            }
+            else
+                MessageBox.Show("暂时不支持");
+            return null;
+        }
+
+        
+        private void SetModels(string API,string proxyIP,string proxyPort)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Ssl3 | System.Net.SecurityProtocolType.Tls12 | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls;
+            string proxyAddress = $"http://{proxyIP}:{proxyPort}";
+            WebProxy proxy = new WebProxy(proxyAddress, true);
+            WebRequest.DefaultWebProxy = proxy;
+            string apiEndpoint = "https://api.openai.com/v1/models";
+            var  request = WebRequest.Create(apiEndpoint);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Headers.Add("Authorization", "Bearer " + API);
+
+            var response = request.GetResponse();
+            StreamReader streamReader = new StreamReader(response.GetResponseStream());
+            string sJson = streamReader.ReadToEnd();
+
+            cbModel.Items.Clear();
+
+            SortedList oSortedList = new SortedList();
+            System.Web.Script.Serialization.JavaScriptSerializer oJavaScriptSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            Dictionary<string, object> oJson = (Dictionary<string, object>)oJavaScriptSerializer.DeserializeObject(sJson);
+            Object[] oList = (Object[])oJson["data"];
+            for (int i = 0; i <= oList.Length - 1; i++)
+            {
+                Dictionary<string, object> oItem = (Dictionary<string, object>)oList[i];
+                string sId = (String) oItem["id"];
+                if (oSortedList.ContainsKey(sId) == false)
+                {
+                    oSortedList.Add(sId, sId);
+                }                
+            }
+
+            foreach (DictionaryEntry oItem in oSortedList)
+                cbModel.Items.Add(oItem.Key);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("不同model请求的URL不同，有可能用不了");
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+            {
+                MessageBox.Show("先输入必填数据");
+            }
+            else
+                SetModels(textBox1.Text,textBox2.Text,textBox3.Text); 
+        }
+
+        private void ChatGPT_Load(object sender, EventArgs e)
+        {
+            cbModel.SelectedIndex = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            messageHistory.Clear();
+            richTextBox2.Clear();
+            label5.Text = "等待中";
+        }
+    }
+}
